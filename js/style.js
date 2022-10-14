4ed7aa4c8ef4f96ac4e272f99871dc36c07e8631
Gleb952 <lomakin.plex@mail.ru>
2022-10-14 18:51:22 +0300

    function calculate for each/ the end

--- js/style.js
+++ js/style.js
@@ -1,10 +1,19 @@
-var money = document.getElementById("inputMoney").value;
-var people = document.getElementById("inputPeople").value;
-var servis = document.getElementById("inputServis").value;
+function calculate() {
 
-function process() {
-    var tip = money * 100 / servis;
-    var tipPeople = tip / people;
-    var tipTotal = tip + tipPeople;
-    print(tipTotal);
+    var money = document.getElementById("inputMoney").value;
+    var people = document.getElementById("inputPeople").value;
+    var servis = document.getElementById("inputServis").value;
+
+    var tip = ((money * servis) / (people));
+
+    if (tip === "none") {
+        tip = "Tip $0 each";
+        alert(tip);
+
+    } else {
+        tip = "Tip $" + tip + " each";
+        alert(tip);
+
+    }
+
 }
